e3421222a40fc204d03f224df8b62bc8e7627258
[dyad] <gcfilhosdofogo@gmail.com>
2025-12-17 02:40:30 -0300

    [dyad] Ajustando a configuração do PostCSS para CommonJS - wrote 1 file(s) + extra files edited outside of Dyad

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-export default {
+// postcss.config.js
+module.exports = {
   plugins: {
-    tailwindcss: tailwindcss,
-    autoprefixer: autoprefixer,
+    tailwindcss: require('tailwindcss'),
+    autoprefixer: require('autoprefixer'),
   },
 };
