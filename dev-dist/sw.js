48c71376360c5dccbd59ecb6a9e5ac0027af58f0
gcfilhosdofogo-hub <gcfilhosdofogo@gmail.com>
2026-01-12 04:50:36 +0000

    ignora

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gt37hfknjlo"
+    "revision": "0.glu4677td7"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
