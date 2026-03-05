df65b0c1a0cfdd96cba21883f9aaca5e83a266bb
gcfilhosdofogo-hub <gcfilhosdofogo@gmail.com>
2026-03-05 16:20:48 +0000

    sw

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qreqrpiolrg"
+    "revision": "0.p74nf1qvkoo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
